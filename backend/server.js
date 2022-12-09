0b3f98863cda6bac9db2d9cb71d1e4d271d211dc
lil-sahil <sahilnagpal93@yahoo.ca>
2022-12-08 22:12:48 -0500

    Goal routes and controllers created. Logic not yet complete for controllers.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
+app.use('/api/goals', require('./routes/goalRoutes'))
+
 app.listen(PORT, () => {
     console.log(`Server started on port number: ${PORT}`)
 })
