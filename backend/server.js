49dd4ebca896ba7116d14396cc5e42e90fda46b8
lil-sahil <sahilnagpal93@yahoo.ca>
2022-12-08 22:27:53 -0500

    Middleware added to handle encoding and error handling.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const dotend = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server started on port number: ${PORT}`)
