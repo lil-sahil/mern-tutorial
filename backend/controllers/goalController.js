b512063f6d94e0d56a5a86669ab512963cd68168
lil-sahil <sahilnagpal93@yahoo.ca>
2022-12-10 14:42:42 -0500

    User model and authentication added.

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,12 @@
 const asyncHandler = require('express-async-handler')
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({user: req.user.id})
     res.status(200).json(goals)
 })
 
@@ -19,7 +20,8 @@ const setGoal = asyncHandler(async (req, res) => {
     }
 
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     })
     res.status(200).json(goal)
 })
@@ -34,6 +36,18 @@ const updateGoal = asyncHandler(async (req, res) => {
     if (!goal){
         res.status(400)
         throw new Error("Goal not found.")
+    }
+
+    const user = await User.findById(req.user.id)
+
+    if(!user) {
+        res.status(401)
+        throw new Error("User not found")
+    }
+
+    if(goal.user.toString() != user.id){
+        res.status(401)
+        throw new Error("User not authorized")
     }
 
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
@@ -53,6 +67,18 @@ const deleteGoal = asyncHandler(async (req, res) => {
     if (!goal){
         res.status(400)
         throw new Error("Goal not found.")
+    }
+
+    const user = await User.findById(req.user.id)
+
+    if(!user) {
+        res.status(401)
+        throw new Error("User not found")
+    }
+
+    if(goal.user.toString() != user.id){
+        res.status(401)
+        throw new Error("User not authorized")
     }
 
     const deletedGoal = await Goal.findByIdAndDelete(req.params.id)
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const {getGoals, setGoal, updateGoal, deleteGoal} = require('../controllers/goalController')
+const {protect} = require('../middleware/authMiddleware')
 
-router.route('/').get(getGoals).post(setGoal)
+
+router.route('/').get(protect, getGoals).post(protect, setGoal)
 // Below routes are the same as above
 // router.get('/', getGoals)
 
 // router.post('/', setGoal)
 
-router.route('/:id').put(updateGoal).delete(deleteGoal)
+router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoal)
 
 // Below routes are the same as above
 // router.put('/:id', updateGoal)
