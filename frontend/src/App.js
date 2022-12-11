0f05ba360ab085f668c08ff3c0a421154001293f
lil-sahil <sahilnagpal93@yahoo.ca>
2022-12-11 15:38:13 -0500

    Front ui, login, register pages and header component created.

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+// Pages
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+// Components
+import Header from './components/Header/Header';
+
+
+
+function App() {
+  return (
+    
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
