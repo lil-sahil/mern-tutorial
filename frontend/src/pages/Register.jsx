1f3d32e8cc0e9203480492811d2c763b59a79492
lil-sahil <sahilnagpal93@yahoo.ca>
2022-12-11 22:46:26 -0500

    Set up slice and reducers, and extra reducers for user authentication. Register logic complete.

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react"
 import {FaUser} from 'react-icons/fa'
+import {useNavigate} from "react-router-dom"
+import {toast} from 'react-toastify'
+// Redux
+import {useSelector, useDispatch} from 'react-redux'
+import { register, reset } from "../features/auth/authSlice"
+
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -11,6 +17,26 @@ const Register = () => {
 
   const {name, email, password, password2} = formData
 
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const {user, isLoading, isError, isSuccess, message} = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if(isError){
+        toast.error(message)
+    }
+
+    if(isSuccess || user){
+        navigate('/')
+    }
+
+    dispatch(reset())
+    
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
   const onChange = (e) => {
     setFormData(prevState => ({
         ...prevState,
@@ -20,6 +46,18 @@ const Register = () => {
 
   const onSubmit = (e) => {
     e.preventDefault()
+
+    if (password !== password2) {
+        toast.error("Passwords do not match")
+    } else {
+        const userData = {
+            name, 
+            email,
+            password
+        }
+
+        dispatch(register(userData))
+    }
   }
   
   return (
